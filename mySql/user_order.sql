66ece742dcc51d606f06f1085f84229c043777a4
Your Name <s19910507@gmail.com>
2021-06-30 16:37:35 +0800

    user_order commit

---
+++ mySql/user_order.sql
@@ -0,0 +1,62 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- 主機： 127.0.0.1
+-- 產生時間： 2021-06-30 10:21:10
+-- 伺服器版本： 10.4.19-MariaDB
+-- PHP 版本： 8.0.7
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- 資料庫: `mfee17_4_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `user_order`
+--
+
+CREATE TABLE `user_order` (
+  `id` int(6) UNSIGNED NOT NULL,
+  `product_id` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `user_id` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `price` varchar(10) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `amount` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `order_date` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- 已傾印資料表的索引
+--
+
+--
+-- 資料表索引 `user_order`
+--
+ALTER TABLE `user_order`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- 在傾印的資料表使用自動遞增(AUTO_INCREMENT)
+--
+
+--
+-- 使用資料表自動遞增(AUTO_INCREMENT) `user_order`
+--
+ALTER TABLE `user_order`
+  MODIFY `id` int(6) UNSIGNED NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
