1d081089628060506c934d5d2b465c37755f0222
Sarah <hsue1207@gmail.com>
2021-07-05 04:50:56 +0800

    add

--- mySql/user_order.sql
+++ mySql/user_order.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- 主機： 127.0.0.1
--- 產生時間： 2021-06-30 10:21:10
+-- 產生時間： 2021-07-04 14:33:02
 -- 伺服器版本： 10.4.19-MariaDB
 -- PHP 版本： 8.0.7
 
@@ -28,12 +28,13 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `user_order` (
-  `id` int(6) UNSIGNED NOT NULL,
-  `product_id` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `user_id` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `price` varchar(10) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `amount` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `order_date` datetime NOT NULL
+  `order_id` int(10) UNSIGNED NOT NULL,
+  `product_id` int(6) UNSIGNED NOT NULL,
+  `user_id` int(6) UNSIGNED NOT NULL,
+  `invoice_no` int(50) UNSIGNED NOT NULL,
+  `total` int(5) UNSIGNED NOT NULL,
+  `order_date` datetime NOT NULL,
+  `order_status` text COLLATE utf8mb4_unicode_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
@@ -44,7 +45,7 @@ CREATE TABLE `user_order` (
 -- 資料表索引 `user_order`
 --
 ALTER TABLE `user_order`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`order_id`);
 
 --
 -- 在傾印的資料表使用自動遞增(AUTO_INCREMENT)
@@ -54,7 +55,7 @@ ALTER TABLE `user_order`
 -- 使用資料表自動遞增(AUTO_INCREMENT) `user_order`
 --
 ALTER TABLE `user_order`
-  MODIFY `id` int(6) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `order_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
