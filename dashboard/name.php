815a5f65a9e4740643227b44d065b97e921bc108
TaylorLiu11 <83996318+TaylorLiu11@users.noreply.github.com>
2021-07-05 15:45:17 +0800

    Update name.php

--- dashboard/name.php
+++ dashboard/name.php
@@ -1,4 +1,4 @@
-<?php require_once("header.php"); ?>
+<?php require_once("includes/header.php"); ?>
 
 <!-- page content -->
       <div class="right_col" role="main">
@@ -56,4 +56,4 @@
 
 
 
-<?php require_once("footer.php"); ?>
+<?php require_once("includes/footer.php"); ?>
