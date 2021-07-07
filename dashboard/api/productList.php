35ce932e170ffd12106397a9d1e97aef442199ae
Reverie <83707139+h221391ba@users.noreply.github.com>
2021-07-07 15:42:08 +0800

    api增加註解

--- dashboard/api/productList.php
+++ dashboard/api/productList.php
@@ -16,19 +16,19 @@ try {
     $typeArr = [];
     $jArr = [];
     foreach ($typeRows as $row) {
-        $typeArr[$row['id']] = $row['type_value'];
+        $typeArr[$row['id']] = $row['type_value'];                  //以id作為key產生新的陣列以便後續取值
     }
     foreach ($rows as $row) {
         $typeStrArr = [];
-        $arr = explode(',', $row['sku_group']);
+        $arr = explode(',', $row['sku_group']);                     //字串分割變成陣列
         foreach ($arr as $value) {
             if (!empty($value)) {
-                array_push($typeStrArr, $typeArr[$value]);
+                array_push($typeStrArr, $typeArr[$value]);          //陣列的值變成key並得出相對值 xe:1 => 黑
             } else {
-                array_push($typeStrArr, " ");
+                array_push($typeStrArr, "");                        //有些資料的type_value為空會錯誤 因此判斷後給空字串
             }
         }
-        $typeStr = implode(' ', $typeStrArr);
+        $typeStr = implode(' ', $typeStrArr);                       //將新陣列串 之後傳到$arr2裡面
         $arr2 = [
             'product_id' => $row['id'],
             'product_name' => $row['name'],
@@ -39,14 +39,14 @@ try {
             'upload_time' => $row['creat_time'],
             'status' => $row['product_sku_status']
         ];
-        array_push($jArr, $arr2);
+        array_push($jArr, $arr2);                                   //將每一row(array)傳到一個陣列中
     }
 //    var_dump($jArr);
 //foreach ($jArr as $value){
 //    print_r($value);
 //    echo '<br>';
 //}
-    echo json_encode($jArr);
+    echo json_encode($jArr);                                        //形成json檔
 } catch (PDOException $e) {
     echo 'database connection error : <br>' . $e->getMessage() . '<br>';
     exit();
