5458aab407157d7fc1ddddbd1fe0bab300ff0c13
Sarah <hsue1207@gmail.com>
2021-07-13 16:13:55 +0800

    新增 刪除功能

---
+++ dashboard/userOrderDelete.php
@@ -0,0 +1,24 @@
+<?php
+require_once('includes/config.php');
+
+$id = $_GET["id"];
+
+$userOrderSql = "UPDATE user_order SET valid = 0 WHERE id = ?";
+
+$userOrderStmt = $db_host->prepare($userOrderSql);
+
+try{
+    $userOrderStmt->execute([$id]);
+    header("location: user_order.php");
+
+}catch (PDOException $e){
+    echo "資料庫連結失敗<br>";
+    echo "Error: ".$e -> getMessage(). "<br>";
+    exit;
+}
+// echo "資料庫連結成功";
+
+// 資料庫操作完, 再將資料庫連結關閉
+// $db_host=null;
+
+?>
