fa846ed8b8645a57661e53e2d70a7cbcc71cd7f4
Sarah <hsue1207@gmail.com>
2021-07-18 11:45:14 +0800

    user_order改成scss, user_order增加分頁

--- dashboard/js/userOrder.js
+++ dashboard/js/userOrder.js
@@ -1,3 +1,5 @@
+// 利用js做分頁
+
 axios({
     method: 'post',
     url: '/project_01/dashboard/api/userOrder.php',
@@ -7,24 +9,30 @@ axios({
     // console.log(data);
     let content = "";
     // console.log(data);
-    data.forEach((order) => {
+
+    // 總訂單筆數
+    $("#orderCount").text(`共有${data.length}張訂單`);
+    console.log($("#orderCount").text());
+
+    // 起始頁1~10筆資料
+    for (let i = 0; i < 10; i++)
         content += `
             <tr class="even pointer">
                 <!-- <td class="align-middle">
                   <input type="checkbox" class="flat" name="table_records">
                 </td> -->
-                <td class="align-middle text-center">${order.order_date}</td>
-                <td class="align-middle text-center"><a href="user_order_detail.php?order_id=${order.order_id}">${order.order_no}</a></td>
-                <td class="align-middle text-center">${order.user_account}</td>
-                <td class="align-middle text-center">${order.phone}</td>
-                <td class="align-middle text-center">${order.address}</td>
-                <td class="align-middle text-center">${order.paytype}</td>
-                <td class="align-middle text-center">${order.delivery}</td>
-                <td class="align-middle text-center">${order.order_status}</td>
-                <td class="align-middle text-center">${order.invoice_no}</td>
+                <td class="align-middle text-center">${data[i].order_date}</td>
+                <td class="align-middle text-center"><a href="user_order_detail.php?order_id=${data[i].order_id}">${data[i].order_no}</a></td>
+                <td class="align-middle text-center">${data[i].user_account}</td>
+                <td class="align-middle text-center">${data[i].phone}</td>
+                <td class="align-middle text-center">${data[i].address}</td>
+                <td class="align-middle text-center">${data[i].paytype}</td>
+                <td class="align-middle text-center">${data[i].delivery}</td>
+                <td class="align-middle text-center">${data[i].order_status}</td>
+                <td class="align-middle text-center">${data[i].invoice_no}</td>
                 <td class=" p-2">
                         <div class="d-flex justify-content-center">
-                            <a href="user_order_update.php?id=${order.order_id}"
+                            <a href="user_order_update.php?id=${data[i].order_id}"
                                class="btn btn-round btn-secondary d-flex justify-content-center align-items-center"
                                style="width: 30px;height: 30px;" title="編輯"><i class="fas fa-edit"></i></a>
                             <a class="btn btn-round btn-danger d-flex justify-content-center align-items-center text-white"
@@ -44,7 +52,7 @@ axios({
                                 </div>
                                 <div class="modal-footer">
                                     <a type="button" class="btn btn-secondary text-white" data-dismiss="modal">取消</a>
-                                    <a href="userOrderDelete.php?id=${order.order_id}" type="button" class="btn btn-primary">確認</a>
+                                    <a href="userOrderDelete.php?id=${data[i].order_id}" type="button" class="btn btn-primary">確認</a>
                                 </div>
                                 </div>
                             </div>
@@ -52,8 +60,86 @@ axios({
                     </td>
             </tr>
             `
-    })
+
+    // })
     $("tbody").append(content);
+
+    // 分頁
+    let pageContent = "";
+    let perPage = 10;
+    let totalPage = Math.ceil(data.length / perPage);
+    console.log(totalPage);
+
+    for (let i = 1; i <= totalPage; i++) {
+        pageContent += `
+            <li id="thisPage" class="page-item" data-page="${i}"><a id="" class="page-link">${i}</a></li>
+            `
+    }
+    $("#previous").after(pageContent);
+
+    $("#orderListPage").on("click", "#thisPage", function(){
+        let page = $(this).data("page");   // $(this)是指pageContent的<li>
+        $(this).addClass('active').siblings().removeClass('active');
+        console.log(page);
+        content = "";
+        $("tbody").empty();
+        
+        let pageStart = (page - 1) * perPage;
+        let pageChange = (page * perPage);
+        // console.log(pageChange);
+
+        if (page == totalPage) {
+            pageChange = pageStart + data.length % perPage;
+        }
+
+        for (let i = pageStart; i < pageChange; i++) {
+            content += `
+            <tr class="even pointer">
+                <!-- <td class="align-middle">
+                  <input type="checkbox" class="flat" name="table_records">
+                </td> -->
+                <td class="align-middle text-center">${data[i].order_date}</td>
+                <td class="align-middle text-center"><a href="user_order_detail.php?order_id=${data[i].order_id}">${data[i].order_no}</a></td>
+                <td class="align-middle text-center">${data[i].user_account}</td>
+                <td class="align-middle text-center">${data[i].phone}</td>
+                <td class="align-middle text-center">${data[i].address}</td>
+                <td class="align-middle text-center">${data[i].paytype}</td>
+                <td class="align-middle text-center">${data[i].delivery}</td>
+                <td class="align-middle text-center">${data[i].order_status}</td>
+                <td class="align-middle text-center">${data[i].invoice_no}</td>
+                <td class=" p-2">
+                        <div class="d-flex justify-content-center">
+                            <a href="user_order_update.php?id=${data[i].order_id}"
+                               class="btn btn-round btn-secondary d-flex justify-content-center align-items-center"
+                               style="width: 30px;height: 30px;" title="編輯"><i class="fas fa-edit"></i></a>
+                            <a class="btn btn-round btn-danger d-flex justify-content-center align-items-center text-white"
+                               style="width: 30px;height: 30px;" id="delete" title="刪除" data-toggle="modal" data-target="#exampleModal">
+                               <i class="fas fa-trash-alt"></i>
+                               </a>
+                        </div>
+                        <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                            <div class="modal-dialog modal-dialog-centered">
+                                <div class="modal-content">
+                                <div class="modal-header">
+                                    <h5 class="modal-title" id="exampleModalLabel">確認刪除訂單?</h5>
+                                    </button>
+                                </div>
+                                <div class="modal-body">
+                                    一經刪除無法復原 !
+                                </div>
+                                <div class="modal-footer">
+                                    <a type="button" class="btn btn-secondary text-white" data-dismiss="modal">取消</a>
+                                    <a href="userOrderDelete.php?id=${data[i].order_id}" type="button" class="btn btn-primary">確認</a>
+                                </div>
+                                </div>
+                            </div>
+                        </div>
+                    </td>
+            </tr>
+            `
+        }
+        $("tbody").append(content);
+    })
 
 }).catch(function (error) {
     console.log(error);
--- dashboard/user_order.php
+++ dashboard/user_order.php
@@ -3,112 +3,121 @@ $title = '訂單資訊';
 $style = 'userOrder.css';
 $js = 'userOrder.js';
 require_once('includes/header.php');
+// require_once('userOrderPage.php');
 
-$sql = 'SELECT * FROM user_order'; // WHERE order_status = 
-$stmt = $db_host->prepare($sql);
-$stmt->execute();
-$count = $stmt->rowCount();
+
 ?>
 
 <!-- page content -->
 <div class="right_col" role="main">
-  <div class="">
-    <div class="page-title">
-      <div class="title_left">
-        <h3>訂單</h3>
-      </div>
-
-      <div class="title_right">
-        <div class="col-md-5 col-sm-5   form-group pull-right top_search">
-          <div class="input-group">
-            <input type="text" class="form-control" placeholder="Search for...">
-            <span class="input-group-btn">
-              <button class="btn btn-default" type="button">Go!</button>
-            </span>
-          </div>
-        </div>
-      </div>
-    </div>
-
-    <div class="clearfix"></div>
-
-    <div class="row">
-      <div class="col-md-12 col-sm-12">
-        <div class="x_panel">
-          <div class="x_title m-0 d-flex">
-            <h2 class="flex-grow-1">訂單列表<small></small></h2>
-            <a class="btn btn-dark text-nowrap m-0 border-0 text-white" style="border-radius:5px; font-size:14px" href="user_order_create.php" type="submit"><i class="fas fa-plus mr-2"></i>新增訂單</a>
-            <div class="clearfix"></div> <!-- 分隔線偽元素 -->
-          </div>
-
-          <div class="x_content">
-            <div class="d-flex py-2 align-items-center">
-              <p class="flex-grow-1 m-0">共有<?= $count ?>張訂單</p>
-              <div>
-                <form class="d-flex form-control border-0 m-0 p-0" style="height:33px;" action="user_order.php">
-                  <input type="date" class="form-control" style="height:33px;" name="start">
-                  <input type="date" class="form-control" style="height:33px;" name="end">
-                  <button class="btn btn-dark text-nowrap mx-2 my-0 border-0" style="border-radius:5px; font-size:14px" type="submit">選擇</button>
-                </form>
-              </div>
-              <select name="" id="" class="form-control text-white" style="width:120px; background:rgba(52,73,94,0.94); border-radius:5px; font-size:14px">
-                <option value="0" selected>訂單狀態</option>
-                <option value="1">待出貨</option>
-                <option value="2">已出貨</option>
-                <option value="3">取消訂單</option>
-              </select>
-
+    <div class="">
+        <div class="page-title">
+            <div class="title_left">
+                <h3>訂單</h3>
             </div>
 
-            <div class="table-responsive">
-              <table class="table table-striped jambo_table bulk_action">
-                <thead>
-                  <tr class="headings">
-                    <!-- <th>
-                      <input type="checkbox" id="check-all" class="flat">
-                    </th> -->
-                    <th class="align-middle text-center">
-                      訂購日
-                      <i class="fas fa-sort"></i>
-                    </th>
-                    <th class="align-middle text-center">
-                      訂單編號
-                      <i class="fas fa-sort"></i>
-                    </th>
-                    <th class="align-middle text-center">訂購人</th>
-                    <th class="align-middle text-center">連絡電話</th>
-                    <th class="align-middle text-center">收件地址</th>
-                    <th class="align-middle text-center">付款方式</th>
-                    <th class="align-middle text-center">運送方式</th>
-                    <th class="align-middle text-center"><span class="nobr">訂單狀態</span>
-                    <th class="align-middle text-center">發票號碼</th>
-                    </th>
-                    <th class="align-middle text-center">操作</th>
-                    </th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <!-- <tr class="even pointer">
-                      <td class="a-center ">
-                        <input type="checkbox" class="flat" name="table_records">
-                      </td>
-                      <td></td>
-                      <td></td>
-                      <td></td>
-                      <td></td>
-                      <td></td>
-                      <td></td>
-                      <td></td>
-                      <td></td>
-                    </tr> -->
-                </tbody>
-              </table>
+            <div class="title_right">
+                <div class="col-md-5 col-sm-5   form-group pull-right top_search">
+                    <div class="input-group">
+                        <input type="text" class="form-control" placeholder="Search for...">
+                        <span class="input-group-btn">
+                            <button class="btn btn-default" type="button">Go!</button>
+                        </span>
+                    </div>
+                </div>
             </div>
-          </div>
         </div>
-      </div>
+
+        <div class="clearfix"></div>
+
+        <div class="row">
+            <div class="col-md-12 col-sm-12">
+                <div class="x_panel">
+                    <div class="x_title m-0 d-flex">
+                        <h2 class="flex-grow-1">訂單列表<small></small></h2>
+                        <a class="btn btn-dark text-nowrap m-0 border-0 text-white" style="border-radius:5px; font-size:14px" href="user_order_create.php" type="submit"><i class="fas fa-plus mr-2"></i>新增訂單</a>
+                        <div class="clearfix"></div> <!-- 分隔線偽元素 -->
+                    </div>
+
+                    <div class="x_content">
+                        <div class="d-flex py-2 align-items-center">
+                            <p class="flex-grow-1 m-0" id="orderCount"></p>
+                            <div>
+                                <form class="d-flex form-control border-0 m-0 p-0" style="height:33px;" action="user_order.php">
+                                    <input type="date" class="form-control" style="height:33px;" name="start">
+                                    <input type="date" class="form-control" style="height:33px;" name="end">
+                                    <button class="btn btn-dark text-nowrap mx-2 my-0 border-0" style="border-radius:5px; font-size:14px" type="submit">選擇</button>
+                                </form>
+                            </div>
+                            <select name="" id="" class="form-control text-white" style="width:120px; background:rgba(52,73,94,0.94); border-radius:5px; font-size:14px">
+                                <option value="0" selected>訂單狀態</option>
+                                <option value="1">待出貨</option>
+                                <option value="2">已出貨</option>
+                                <option value="3">取消訂單</option>
+                            </select>
+
+                        </div>
+
+                        <div class="table-responsive">
+                            <table class="table table-striped jambo_table bulk_action">
+                                <thead>
+                                    <tr class="headings">
+                                        <!-- <th><input type="checkbox" id="check-all" class="flat"></th> -->
+                                        <th class="align-middle text-center">
+                                            訂購日
+                                            <!-- <i class="fas fa-sort"></i> -->
+                                        </th>
+                                        <th class="align-middle text-center">
+                                            訂單編號
+                                            <!-- <i class="fas fa-sort"></i> -->
+                                        </th>
+                                        <th class="align-middle text-center">訂購人</th>
+                                        <th class="align-middle text-center">連絡電話</th>
+                                        <th class="align-middle text-center">收件地址</th>
+                                        <th class="align-middle text-center">付款方式</th>
+                                        <th class="align-middle text-center">運送方式</th>
+                                        <th class="align-middle text-center">訂單狀態</th>
+                                        <th class="align-middle text-center">發票號碼</th>
+                                        <th class="align-middle text-center">操作</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <!-- <tr class="even pointer">
+                                        <td class="a-center ">
+                                            <input type="checkbox" class="flat" name="table_records">
+                                        </td>
+                                        <td></td>
+                                        <td></td>
+                                        <td></td>
+                                        <td></td>
+                                        <td></td>
+                                        <td></td>
+                                        <td></td>
+                                        <td></td>
+                                    </tr> -->
+                                </tbody>
+                            </table>
+                        </div>
+                        <nav aria-label="Page navigation example">
+                            <ul class="pagination justify-content-center" id="orderListPage">
+                                <li class="page-item" id="previous">
+                                    <a class="page-link" href="user_order.php" aria-label="Previous">
+                                        <i class="fa fa-angle-double-left text-secondary"></i>
+                                    </a>
+                                </li>
+                                <!-- <li class="page-item text-secondary"><a class="page-link" href="#">1</a></li> -->
+                                <li class="page-item"  id="Next">
+                                    <a class="page-link" href="#" aria-label="Next">
+                                        <i class="fa fa-angle-double-right text-secondary"></i>
+                                    </a>
+                                </li>
+                            </ul>
+                        </nav>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
-  </div>
 </div>
 
 <!-- /page content -->
